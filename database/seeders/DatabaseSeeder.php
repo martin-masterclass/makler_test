7e8f518f34912338d27ccdf005db825d77d177b0
Martin Krebs Eberth <>
2022-05-21 11:38:13 +0200

    Admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        // Admin-User (Du!)
+        User::create(
+            [
+                'name' => 'Martin',
+                'email' => 'martin@laravel-php.com',
+                'password' => bcrypt('bla123')
+            ]
+        );
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
